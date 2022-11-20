2d6b692b688197c44b2e48adf8728afa1ecb159f
Thim7 <pqhuy.lbapag@gmail.com>
2022-11-20 15:40:53 +0700

    course update

--- memo-zone/src/App.js
+++ memo-zone/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Learn from "./pages/Learn/Learn";
 import Login from "./pages/Login/Login";
+import Course from "./pages/Course/Course";
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="*" name="Home" element={<Login />} />
+          <Route path="*" name="Home" element={<Course />} />
           {/* <Route path="wordtopic" name="Home" element={<WordTopic />} /> */}
         </Routes>
       </BrowserRouter>
--- memo-zone/src/pages/Course/Course.js
+++ memo-zone/src/pages/Course/Course.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import AppHeader from "../../components/AppHeader";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-
+import Scss_Course from"./Scss_Course.scss";
 // giả sử đã select được course từ trang trước đó
 import data from "../../data";
 const Course = () => {
@@ -16,15 +16,26 @@ const Course = () => {
       <AppHeader />
       <div className="course-container">
         <div className="course-info">
-          <div>Tên khóa học: {data[0].courseName}</div>
-          <div>
+          <div className="course-name">{data[0].courseName}</div>
+          <div className="course-description text">
             Info: Phần 6 của nhiệm vụ đầy cảm hứng và hoàn chỉnh đến Memrise
             Toeic. Tranh luận trong tiếng Anh mà không sợ bị lép vế, nhặt nhạnh
             các lối nói gây cảm hứng, và nói như người bản địa với những câu đùa
             dí dỏm đa dụng.
           </div>
         </div>
-        <div className="course-list">
+        <div className="levels clearfix">
+        <a className="level clearfix" href ="#">
+          <div className="level-icon">
+            <i className="level-ico"></i>
+          </div>
+          <div className="level-title">Chủ đề môn học</div>
+        </a>
+        </div>
+  
+
+  
+        {/* <div className="course-list">
           <div className="word-info">
             <div>Eng: {word.eng}</div>
             <div>Pronounce: {word.pronounce}</div>
@@ -33,7 +44,7 @@ const Course = () => {
           <div style={{ color: "red" }} onClick={nextWordHandle}>
             Next
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
