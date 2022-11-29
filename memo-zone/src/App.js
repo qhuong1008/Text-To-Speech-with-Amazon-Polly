2e09fde607f9cfe204db7d824f2e354d2367df90
{QuynhHuong} <qhuong1008@gmail.com>
2022-11-29 17:03:03 +0700

    practice common

--- memo-zone/src/App.js
+++ memo-zone/src/App.js
@@ -4,15 +4,16 @@ import Learn from "./pages/Learn/Learn";
 import Xong from "./pages/Xong";
 import Login from "./pages/Login/Login";
 import Course from "./pages/Course/Course";
+import PracticeCommon from "./pages/Practice/PracticeCommon";
 import { formatCountdown } from "antd/lib/statistic/utils";
+
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Routes>
           <Route path="*" name="Home" element={<Learn />} />
-          <Route path="/xong" element={<Xong />} />
-          {/* <Route path="wordtopic" name="Home" element={<WordTopic />} /> */}
         </Routes>
       </BrowserRouter>
     );
