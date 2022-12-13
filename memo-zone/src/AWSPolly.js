9597ef524df69dcd25a335fab463a68ddbdae417
LienNTB <94447923+LienNTB@users.noreply.github.com>
2022-12-13 13:49:01 +0700

    abc

--- memo-zone/src/AWSPolly.js
+++ memo-zone/src/AWSPolly.js
@@ -2,9 +2,9 @@ var AWS = require("aws-sdk");
 var $ = require("jquery");
 AWS.config.region = "us-east-1"; // Region
 AWS.config.credentials = new AWS.Credentials(
-  "ASIASCU3UYOH2PLS74F6",
-  "7/OYOBu3wU7hZTt9DY4/By5NyAMVuWUhzJhbOTbk",
-  "FwoGZXIvYXdzEJz//////////wEaDHvHIvxHB7Is8OkyTiLPAWdZ/6qTiBnnYLIIGyKxlExDnC28v8CMcWgQU7q0GApk/pA8lH9Uprd9xRcwmlke9YSHHRi9h1hDBAFZlCzYOoo7THz+T6vwYAumWx9KCoYODe8lEErqENoCxLWPJKqNacFaiOoLDvfoaNDTiKKJNYGUwY4LWQaMYdrVvGxtS0l0GdJBVlvvzuCbFMKWGN+rTeebk5wWfSEbcGij1IJlyMefyd1eiXMHZw+zBp4WDn0/vD4NlWKHOLDqpJqmzBNKCnUhdy6fS+l+CobexbT57SipvN+cBjItBXi48AMeFEifjrvlCOuDxGlg6XHc8UjNtKmdHKuyw26FDjwRxtQd9kcLvpCf"
+  "ASIAWLTLA44KT5Z3KPEK",
+  "kXV0KBMvSrAUihbvO0wABeJfQ0J+zn6hzwgcI35B",
+  "FwoGZXIvYXdzEJ3//////////wEaDJrPb85apYsTq+Q4HSLPASpcvoJ78o7CgmuAxJPXZK2XfD2rFVrBCPiB+iZMR5UxRbAQoJojY9GMILE13gqv7FskxkMGUXeaStzjJ/fE18ZcAysrptfLtxhPpeOzN68qvm8z7cBFrOv7EWhckEiwlHuu0Io5dezK/UDSIyJtLaVnnHUiI9uiTUVzbqx58qfRkaZkpSez/C0Ngwqk4hqRfAJB2xU9dhgBY9guf5//1sMKzq7FF5pjEJ0gnHogCa0qEdpIFxgUo2xWuUn1AT9uDvb72Bl8FgKUdrtuizcHXyiZ49+cBjItXKuWnsPVGbFq8ONGnQDPX5WBF2zM+oygo/CYQ/sMQgsyiwdC+uf4YzJWRCRQ"
 );
 
 export default AWS;
